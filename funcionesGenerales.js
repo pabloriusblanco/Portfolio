36c31401c2b7b2774795a8c4f19ef8e05be46804
Pablo Rius Blanco <pabloriusblanco@gmail.com>
2020-03-27 19:33:23 -0300

    Agregado los efectos de hover con play y el trigger de los efectos de los iconos de los links

--- funcionesGenerales.js
+++ funcionesGenerales.js
@@ -1,3 +1,31 @@
 function funcionesGenerales() {
-    console.log("funcionesGenerales");
+    listeners();
+}
+
+function listeners() {
+    reproduccionGifs();
+    hoversConectados();
+}
+
+function reproduccionGifs() {
+    let proyectos =  Array.from(document.getElementsByClassName("contenedorgifproyecto"));
+    for (let index = 0; index < proyectos.length; index++) {
+        proyectos[index].addEventListener("click", function () {
+            console.log("proyecto" + index)
+        })
+    }
+}
+
+function hoversConectados(){
+        let botonesLinks = Array.from(document.getElementsByClassName("contenedorlink"));
+        let mouseOverEvento = new Event('mouseover');
+        for (let index = 0; index < botonesLinks.length; index++) {
+            botonesLinks[index].addEventListener("mouseenter", function () {
+                botonesLinks[index].children[0].classList.add("hoverIconos");
+            })
+            botonesLinks[index].addEventListener("mouseleave", function () {
+                botonesLinks[index].children[0].classList.remove("hoverIconos");
+            })
+            
+        }
 }
